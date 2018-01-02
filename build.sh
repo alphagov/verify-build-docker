e9f833bdd4041ac29b04c06c88d993a222473ff0
Rory Irvine <rory.irvine@digital.cabinet-office.gov.uk>
2018-01-02 14:48:58 +0000

    Add debugging

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ for path in */ ; do
     [ -f "${path}/Dockerfile" ] || continue # ...or if there's no Dockerfile
     REPO="$DOCKER_HUB_USR/$(basename "${path}")"
     cd "${path}/"
+    ls
+    if [ $REPO == "govukverify/java8" ]; then echo "matches"; fi
     docker build -t $REPO:$TAG -f Dockerfile
     docker login -u $DOCKER_HUB_USR -p $DOCKER_HUB_PSW
     docker push $REPO:$TAG
