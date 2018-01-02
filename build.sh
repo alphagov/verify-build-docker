1efe52893a410aeda2dd617f6ee6221ba122d79a
Rory Irvine <rory.irvine@digital.cabinet-office.gov.uk>
2018-01-02 15:00:21 +0000

    Add branch logic

--- build.sh
+++ build.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 shopt -s nullglob
+env
 REPO="verify-build"
+TAG="${BUILD_NUMBER}"
 for path in */ ; do
-    [ -d "${path}" ]           || continue # Skip if it's not a directory
+    [ -d "${path}" ]            || continue # Skip if it's not a directory
     [ -f "${path}/Dockerfile" ] || continue # ...or if there's no Dockerfile
     REPO="$DOCKER_HUB_USR/$(basename "${path}")"
     cd "${path}/"
-    ls
-    if [ $REPO == "govukverify/java8" ]; then echo "matches"; fi
-    docker build -t $REPO:$TAG -f Dockerfile
+    docker build -t $REPO:$TAG -f Dockerfile .
     docker login -u $DOCKER_HUB_USR -p $DOCKER_HUB_PSW
+    if [ ${BRANCH_NAME} == "master" ]; then
+        docker tag $REPO:$TAG $REPO:latest
+        docker push $REPO:latest;
+    fi
     docker push $REPO:$TAG
     cd ..
 done
